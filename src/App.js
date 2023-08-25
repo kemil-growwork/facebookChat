9416ea6859e195674037e7953d212c755d540d97
Kemil Dhanani <kemil@growworkinfotech.com>
2023-08-25 17:45:03 +0530

    changes fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+"use client";
+import logo from "./logo.svg";
+import "./App.css";
+import { FacebookProvider, CustomChat } from "react-facebook";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FacebookProvider appId="1439262506867166" chatSupport>
+        <CustomChat pageId="116330111561365" minimized={true} />
+      </FacebookProvider>
     </div>
   );
 }
